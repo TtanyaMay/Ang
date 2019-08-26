65c073ecdae1d1570814245b9ea06665b2c90830
TtanyaMay <mail_77@i.ua>
2019-08-26 13:26:35 +0300

    11

--- src/app/add-car/add-car.component.ts
+++ src/app/add-car/add-car.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-add-car',
@@ -6,22 +6,29 @@ import { Component, OnInit, EventEmitter, Output} from '@angular/core';
   styleUrls: ['./add-car.component.css']
 })
 export class AddCarComponent implements OnInit {
-  carName = '';
+  /* carName = ''; *//* (1) */
   carYear = 2017;
-  @Output() onAddCar = new EventEmitter<{name:string, year:number}>();
-
+  @Output('onAddCar')  carEmitter= new EventEmitter<{name:string, year:number}>();
+  @ViewChild('carYearInput', {static: false}) carYearInput: ElementRef /* (2) */
   constructor() { }
 
   ngOnInit() {
   }
 
-  addCar(){
-    this.onAddCar.emit({
+  /* Реализация two way biding  без ngModel  */
+  addCar(carNameEl: HTMLInputElement){
+
+    /* (1) *//* this.onAddCar.emit({
       name: this.carName,
       year: this.carYear
-    })
-    this.carName='';
-    this.carYear = 2017;
+    }) */
+    this.carEmitter.emit({
+      name: carNameEl.value,
+      year: +this.carYearInput.nativeElement.value /* (2) */
+    });
+
+    carNameEl.value='';
+    this.carYearInput.nativeElement.value = 2017; /* (2) */
 
   }
 
